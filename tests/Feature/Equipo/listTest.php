5a7b2fd20d11c28ab454d36af414da5dfcee04ce
apvalencia92 <apvalencia92@gmail.com>
2019-01-29 06:32:35 -0500

    creacion rama modulo equipo

---
+++ tests/Feature/Equipo/listTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature\Equipo;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class listTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
