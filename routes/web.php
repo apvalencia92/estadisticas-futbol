5fffd39d910e85a39e783d47f2a67c6d253144a1
apvalencia92 <apvalencia92@gmail.com>
2019-01-28 21:46:37 -0500

    se agrego la ruta de equipos tipo resource

--- routes/web.php
+++ routes/web.php
@@ -14,28 +14,4 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('usuarios', 'Usercontroller')->middleware('auth');
 Route::put('usuarios/picture/{usuario}', 'UserController@update_image')->name('usuarios.actualizarimagen');
 
-Route::get('equipos',function(){
-   return 'Modulo de equipos';
-});
-
-
-//Route::middleware(['auth'])->prefix('usuarios/')->group(function () {
-//
-//    Route::get('', 'UserController@index')->name('usuarios.index');
-//
-//    Route::post('', 'UserController@store')->name('usuarios.store');
-//
-//    Route::get('create', 'UserController@create')->name('usuarios.create');
-//
-//    Route::get('{user}','UserController@show')->name('usuarios.show');
-//
-//    Route::put('{user}', 'UserController@update')->name('usuarios.update');
-//
-//    Route::delete('{user}', 'UserController@destroy')->name('usuarios.destroy');
-//
-//    Route::get('{user}/edit', 'UserController@edit')->name('usuarios.edit');
-//
-//
-////    Route::put('picture/{user}', 'UserController@update_image')->name('usuarios.actualizarimagen');
-//
-//});
+Route::resource('equipos','EquiposController');
